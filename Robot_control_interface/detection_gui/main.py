2179cf9a54ccb26ba0f7793c9dd99cb996e3b3d8
LBAWMY <bli@mae.cuhk.edu.hk>
2026-03-27 17:38:54 +0800

    code clean

--- Robot_control_interface/detection_gui/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from Mainwindow_ROS_new_gui import MainWindow_ROS
-
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = MainWindow_ROS()
-    MainWindow.show()
-    sys.exit(app.exec_())
--- Robot_control_interface/robots/ur5_vision_msg.py
+++
@@ -1,5 +0,0 @@
-
-class ur5_vision_msg(object):
-    def __init__(self):
-        self.Parameter = None
-        self.ImageError = None
